34132104395855fd9424789533ea897b50d8c4e8
AnandJNairGit <anandjnairit@gmail.com>
2022-10-28 14:23:25 +0530

    import displayUser in postsList

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import DisplayUser from "./DisplayUser";
 import { getPosts } from "./postsSlice";
 
 const PostsList = () => {
   const posts = useSelector(getPosts);
-  console.log(posts);
+
 
   const renderPost = posts.map((post, index) => {
     return (
       <div key={index}>
         <h3>{post.title}</h3>
         <h5>{post.content}</h5>
+        <DisplayUser userId={post.userId} />
         <hr />
       </div>
     );
   });
 
-  return <div>
-    {renderPost}</div>;
+  return <div>{renderPost}</div>;
 };
 
 export default PostsList;
